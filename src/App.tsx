9c417774cd34c5e2e0503db7bc08ad2330e0d053
yoe <yhacaribe@gmail.com>
2022-08-10 00:25:50 -0400

    Fixing token/userId issue on chat.

--- src/App.tsx
+++ src/App.tsx
@@ -21,8 +21,9 @@ import {v4 as uuidv4} from 'uuid';
  * application after the user goes through your authentication flow.
  */
 const ENDPOINT_URL = 'https://chat-quickstart.communication.azure.com/';
-// const USER_ID = '8:acs:a5681390-2336-476e-b7c8-5b87c9fba94a_00000013-1012-b11a-570c-113a0d00bf5e';
-const TOKEN = 'eyJhbGciOiJSUzI1NiIsImtpZCI6IjEwNiIsIng1dCI6Im9QMWFxQnlfR3hZU3pSaXhuQ25zdE5PU2p2cyIsInR5cCI6IkpXVCJ9.eyJza3lwZWlkIjoiYWNzOmI2MzAyNjUwLTljYzYtNDA2Ni1hMzU3LWJhMDM2ZmEzOGY1MF8wMDAwMDAxMy0xZjk0LWMyZGUtNTcwYy0xMTNhMGQwMDBhNzQiLCJzY3AiOjE3OTIsImNzaSI6IjE2NTk5OTk4MjEiLCJleHAiOjE2NjAwODYyMjEsImFjc1Njb3BlIjoiY2hhdCx2b2lwIiwicmVzb3VyY2VJZCI6ImI2MzAyNjUwLTljYzYtNDA2Ni1hMzU3LWJhMDM2ZmEzOGY1MCIsImlhdCI6MTY1OTk5OTgyMX0.JNUSW9yoyjwQiM89xU0FW3jMpbejyRKa-_C3jv03Q4nig8D14q6sEcP-7mWbuRXQCfRvZqv9UUM9rc-A5r1-lxcOWTPN8PHIaYREgNagTLJfNHIZBpDUn0asiyc7ta-IJvDGof-GJQVhaGj2zI-wowh9L6WDh_cp1AqFKbLMCvQC2piJznveK4ZRQTpCth4rCBAnRU8Qz3FOIdm4Y9wXcm4PKq2CQQ41g36Tf7jUJTcjQauV_Z0ZPgoA-OeTW0Kl6HQ7xGQ5nGhwRony4h8fYMtSUKdoJI4oBw5cuK5P0rjbIYQz5cabwj_75uhfL5pndoY4JJ3AE2NmAgbjdxjT8w';
+const CREATOR_USER_ID = '8:acs:b6302650-9cc6-4066-a357-ba036fa38f50_00000013-24b1-ef39-570c-113a0d00826c';
+const DEFAULT_DISPLAY_NAME = 'Chat User Test';
+const CREATOR_TOKEN = 'eyJhbGciOiJSUzI1NiIsImtpZCI6IjEwNiIsIng1dCI6Im9QMWFxQnlfR3hZU3pSaXhuQ25zdE5PU2p2cyIsInR5cCI6IkpXVCJ9.eyJza3lwZWlkIjoiYWNzOmI2MzAyNjUwLTljYzYtNDA2Ni1hMzU3LWJhMDM2ZmEzOGY1MF8wMDAwMDAxMy0yNGIxLWVmMzktNTcwYy0xMTNhMGQwMDgyNmMiLCJzY3AiOjE3OTIsImNzaSI6IjE2NjAwODU2MTkiLCJleHAiOjE2NjAxNzIwMTksImFjc1Njb3BlIjoiY2hhdCx2b2lwIiwicmVzb3VyY2VJZCI6ImI2MzAyNjUwLTljYzYtNDA2Ni1hMzU3LWJhMDM2ZmEzOGY1MCIsImlhdCI6MTY2MDA4NTYxOX0.FydpOCiqTE29O_yU-INr2gF8fsTHjCW8pKxbEHfxC77qvOSjAXpAe0K6SmlBrqpnAKr2HEEF41n8rzDmgmFoIV3j4V_4kINjSExwlJ4JU2mQY11IyB21cIuUhbF_W1juuwZ7kWzaHbwhme9rPrl_ENIgSGw7Fv8lSmZNYe73umv8gEQYVRunuDMKr_dOv7Ktccljwfd20FlpVse-Ic7PFkTv24qRqm2zLg2zlxxyau03r54qIJBqNQopnvLFJ53vdWwuZF4h_fjj4H6oqsU_CJ7kkvyfJbRpNI17gTixpF-vOrKcoRj663IMydFngMupEl9JK167qPECh4MBSzf5KQ';
 
 /**
  * Display name for the local participant.
@@ -37,39 +38,48 @@ const TOKEN = 'eyJhbGciOiJSUzI1NiIsImtpZCI6IjEwNiIsIng1dCI6Im9QMWFxQnlfR3hZU3pSa
 function App(): JSX.Element {
     // Arguments that would usually be provided by your backend service or
     // (indirectly) by the user.
-    const [contextUserId, setContextUserId] = React.useState<string>('');
-    const [contextUserDisplayName, setContextUserDisplayName] = React.useState<string>('');
+    const [initialized, setInitialized] = React.useState<boolean>(false);
+    const [contextUserId, setContextUserId] = React.useState<string>(CREATOR_USER_ID);
+    const [contextUserDisplayName, setContextUserDisplayName] = React.useState<string>(DEFAULT_DISPLAY_NAME);
     const [contextThreadId, setContextThreadId] = React.useState<string>('none');
+    const [contextToken, setContextToken] = React.useState<string>(CREATOR_TOKEN);
     const {
         endpointUrl,
-        userId,
-        token,
-        displayName,
         groupId,
         threadId
-    } = useAzureCommunicationServiceArgs({contextUserId, contextUserDisplayName, contextThreadId});
+    } = useAzureCommunicationServiceArgs({
+        initialized,
+        contextUserId,
+        contextUserDisplayName,
+        contextThreadId
+    });
 
     React.useEffect(() => {
-        const getExistingThreadIdFromURL = (): void => {
+        const getExistingThreadIdFromURL = async (): Promise<void> => {
             const urlParams = new URLSearchParams(window.location.search);
-
             const urlContextThreadId = urlParams.get('threadId');
             if (urlContextThreadId) {
                 console.log("urlContextThreadId context value " + urlContextThreadId);
-                setContextThreadId(urlContextThreadId);
+                await setContextThreadId(urlContextThreadId);
             } else {
                 setContextThreadId('');
             }
             const urlUserId = urlParams.get('userId');
             if (urlUserId) {
                 console.log("urlUserId context value " + urlUserId);
-                setContextUserId(urlUserId);
+                await setContextUserId(urlUserId);
             }
             const urlUserDisplayName = urlParams.get('displayName');
             if (urlUserDisplayName) {
                 console.log("urlUserDisplayName context value " + urlUserDisplayName);
-                setContextUserDisplayName(urlUserDisplayName);
+                await setContextUserDisplayName(urlUserDisplayName);
             }
+            const urlToken = urlParams.get('token');
+            if (urlToken) {
+                console.log("urlToken context value " + urlToken);
+                await setContextToken(urlToken);
+            }
+            setInitialized(true);
         };
         getExistingThreadIdFromURL()
     }, [])
@@ -77,23 +87,23 @@ function App(): JSX.Element {
     // A well-formed token is required to initialize the chat and calling adapters.
     const credential = useMemo(() => {
         try {
-            return new AzureCommunicationTokenCredential(token);
+            return new AzureCommunicationTokenCredential(contextToken);
         } catch {
             console.error('Failed to construct token credential');
             return undefined;
         }
-    }, [token]);
+    }, [contextToken]);
 
     // Memoize arguments to `useAzureCommunicationCallAdapter` so that
     // a new adapter is only created when an argument changes.
     const callAdapterArgs = useMemo(
         () => ({
-            userId: fromFlatCommunicationIdentifier(userId) as CommunicationUserIdentifier,
-            displayName,
+            userId: fromFlatCommunicationIdentifier(contextUserId) as CommunicationUserIdentifier,
+            displayName: contextUserDisplayName,
             credential,
             locator: {groupId}
         }),
-        [userId, credential, displayName, groupId]
+        [contextUserId, credential, contextUserDisplayName, groupId]
     );
     const callAdapter = useAzureCommunicationCallAdapter(callAdapterArgs);
 
@@ -102,16 +112,16 @@ function App(): JSX.Element {
     const chatAdapterArgs = useMemo(
         () => ({
             endpoint: endpointUrl,
-            userId: fromFlatCommunicationIdentifier(userId) as CommunicationUserIdentifier,
-            displayName,
+            userId: fromFlatCommunicationIdentifier(contextUserId) as CommunicationUserIdentifier,
+            displayName: contextUserDisplayName,
             credential,
             threadId
         }),
-        [endpointUrl, userId, displayName, credential, threadId]
+        [endpointUrl, contextUserId, contextUserDisplayName, credential, threadId]
     );
     const chatAdapter = useAzureCommunicationChatAdapter(chatAdapterArgs);
 
-    if (!!callAdapter && !!chatAdapter) {
+    if (initialized && !!callAdapter && !!chatAdapter) {
         return (
             <div style={{height: '100vh', display: 'flex'}}>
                 <div style={containerStyle}>
@@ -126,7 +136,7 @@ function App(): JSX.Element {
     if (credential === undefined) {
         return <h3>Failed to construct credential. Provided token is malformed.</h3>;
     }
-    return <div><h3>Initializing... </h3><span>Enter the required vars in the url</span></div>;
+    return <div><h3>Initializing... </h3></div>;
 }
 
 const containerStyle: CSSProperties = {
@@ -141,23 +151,21 @@ const containerStyle: CSSProperties = {
  * depending on the user-flow (e.g. which chat thread to use).
  */
 function useAzureCommunicationServiceArgs({
+                                              initialized,
                                               contextUserId,
                                               contextUserDisplayName,
                                               contextThreadId
-                                          }: { contextUserId: string, contextUserDisplayName: string, contextThreadId: string }): {
+                                          }: { initialized: boolean, contextUserId: string, contextUserDisplayName: string, contextThreadId: string }): {
     endpointUrl: string;
-    userId: string;
-    token: string;
-    displayName: string;
     groupId: string;
     threadId: string;
 } {
     const [threadId, setThreadId] = useState('');
     // For the quickstart, create a new thread with just the local participant in it.
     useEffect(() => {
-        if (contextUserId && contextUserDisplayName && contextThreadId !== 'none') {
+        if (initialized && contextThreadId !== 'none') {
             (async () => {
-                const client = new ChatClient(ENDPOINT_URL, new AzureCommunicationTokenCredential(TOKEN));
+                const client = new ChatClient(ENDPOINT_URL, new AzureCommunicationTokenCredential(CREATOR_TOKEN));
                 if (!contextThreadId) {
                     const {chatThread} = await client.createChatThread(
                         {
@@ -177,30 +185,19 @@ function useAzureCommunicationServiceArgs({
                 } else {
                     const chatThreadClient = client.getChatThreadClient(contextThreadId);
 
-                    const response = await chatThreadClient.listParticipants();
-                    const participants_ = [];
-                    let prt = await response?.next();
-                    while (!prt?.done) {
-                        participants_.push(prt?.value)
-                        prt = await response?.next();
-                    }
-
                     const addParticipantsRequest: AddParticipantsRequest =
                         {
-                            participants: [
-                                ...participants_,
-                                ...[{
-                                    id: fromFlatCommunicationIdentifier(contextUserId),
-                                    displayName: contextUserDisplayName
-                                }]
-                            ]
+                            participants: [{
+                                id: fromFlatCommunicationIdentifier(contextUserId),
+                                displayName: contextUserDisplayName
+                            }]
                         };
                     await chatThreadClient.addParticipants(addParticipantsRequest);
                     setThreadId(contextThreadId);
                 }
             })();
         }
-    }, [contextUserId, contextUserDisplayName, contextThreadId]);
+    }, [contextThreadId, contextUserDisplayName, contextUserId, initialized]);
 
     // For the quickstart, generate a random group ID.
     // The group Id must be a UUID.
@@ -208,9 +205,6 @@ function useAzureCommunicationServiceArgs({
 
     return {
         endpointUrl: ENDPOINT_URL,
-        userId: contextUserId,
-        token: TOKEN,
-        displayName: contextUserDisplayName,
         groupId: groupId.current,
         threadId
     };
